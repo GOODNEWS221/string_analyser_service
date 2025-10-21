2e33c26e2f2c28e422df4d28a66a2c6948932fb9
Goodnews Atekha Osas <154696005+GOODNEWS221@users.noreply.github.com>
2025-10-21 20:29:45 +0100

    Refactor URL patterns for string views

--- strings/urls.py
+++ strings/urls.py
@@ -7,15 +7,8 @@ from .views import (
 )
 
 urlpatterns = [
-    # Handles both GET and POST
-    path('strings/', StringListCreateView.as_view(), name='list_create_strings'),
-
-    # Retrieve specific string
-    path('strings/<str:value>/', StringRetrieveView.as_view(), name='get_string'),
-
-    # Delete specific string
-    path('strings/<str:value>/', StringDeleteView.as_view(), name='delete_string'),
-
-    # Natural language filter
-    path('strings/filter-by-natural-language/', NaturalLanguageFilterView.as_view(), name='nlp_analysis'),
+    path('strings', StringListCreateView.as_view(), name='list_create_strings'),
+    path('strings/<str:value>', StringRetrieveView.as_view(), name='get_string'),
+    path('strings/<str:value>', StringDeleteView.as_view(), name='delete_string'),
+    path('strings/filter-by-natural-language', NaturalLanguageFilterView.as_view(), name='nlp_analysis'),
 ]
