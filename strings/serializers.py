9cfaec894ce5ecaa7c0f83d672e904b5677b4600
Goodnews Atekha Osas <154696005+GOODNEWS221@users.noreply.github.com>
2025-10-21 20:25:35 +0100

    Simplify create method in StringFileSerializer
    Refactor create method to streamline value assignment and object creation.

--- strings/serializers.py
+++ strings/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import StringFile
 from .utils import string_analysis
 
-
 class StringFileSerializer(serializers.ModelSerializer):
     properties = serializers.SerializerMethodField()
     id = serializers.CharField(read_only=True)
@@ -22,19 +21,6 @@ class StringFileSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
-        value = validated_data['value'].strip()
-
-        # Run string analysis utility (must handle case-insensitive palindrome)
+        value = validated_data['value']
         props = string_analysis(value)
-
-        # Create StringFile record using SHA256 hash as ID
-        return StringFile.objects.create(
-            id=props["sha256_hash"],
-            value=value,
-            length=props["length"],
-            is_palindrome=props["is_palindrome"],
-            unique_characters=props["unique_characters"],
-            word_count=props["word_count"],
-            sha256_hash=props["sha256_hash"],
-            character_frequency_map=props["character_frequency_map"],
-        )
+        return StringFile.objects.create(id=props["sha256_hash"], value=value, **props)
