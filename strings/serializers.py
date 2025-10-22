e4f2a053baaffa24a10990435564bc8e7f2aaf24
Goodnews Atekha Osas <154696005+GOODNEWS221@users.noreply.github.com>
2025-10-22 02:59:10 +0100

    Refactor StringFileSerializer to include new fields

--- strings/serializers.py
+++ strings/serializers.py
@@ -1,26 +1,27 @@
 from rest_framework import serializers
 from .models import StringFile
-from .utils import string_analysis
 
 class StringFileSerializer(serializers.ModelSerializer):
-    properties = serializers.SerializerMethodField()
-    id = serializers.CharField(read_only=True)
-
     class Meta:
         model = StringFile
-        fields = ['id', 'value', 'properties', 'created_at']
+        fields = [
+            "id",
+            "value",
+            "length",
+            "is_palindrome",
+            "unique_characters",
+            "word_count",
+            "sha256_hash",
+            "character_frequency_map",
+            "created_at"
+        ]
+        read_only_fields = ["id", "length", "is_palindrome", "unique_characters", 
+                            "word_count", "sha256_hash", "character_frequency_map", "created_at"]
 
-    def get_properties(self, obj):
-        return {
-            "length": obj.length,
-            "is_palindrome": obj.is_palindrome,
-            "unique_characters": obj.unique_characters,
-            "word_count": obj.word_count,
-            "sha256_hash": obj.sha256_hash,
-            "character_frequency_map": obj.character_frequency_map,
-        }
-
-    def create(self, validated_data):
-        value = validated_data['value']
-        props = string_analysis(value)
-        return StringFile.objects.create(id=props["sha256_hash"], value=value, **props)
+    # Wrap properties in a `properties` key to match Thanos format
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        properties_keys = ["length", "is_palindrome", "unique_characters", 
+                           "word_count", "sha256_hash", "character_frequency_map"]
+        rep["properties"] = {k: rep.pop(k) for k in properties_keys}
+        return rep
