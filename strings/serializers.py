042d1b6bf3d04dd1d01f1dacef691ff816dce8e0
Goodnews Atekha Osas <154696005+GOODNEWS221@users.noreply.github.com>
2025-10-22 03:12:18 +0100

    Refactor StringFileSerializer to include properties

--- strings/serializers.py
+++ strings/serializers.py
@@ -1,27 +1,20 @@
+# serializers.py
 from rest_framework import serializers
 from .models import StringFile
 
 class StringFileSerializer(serializers.ModelSerializer):
+    properties = serializers.SerializerMethodField()
+
     class Meta:
         model = StringFile
-        fields = [
-            "id",
-            "value",
-            "length",
-            "is_palindrome",
-            "unique_characters",
-            "word_count",
-            "sha256_hash",
-            "character_frequency_map",
-            "created_at"
-        ]
-        read_only_fields = ["id", "length", "is_palindrome", "unique_characters", 
-                            "word_count", "sha256_hash", "character_frequency_map", "created_at"]
+        fields = ['id', 'value', 'properties', 'created_at']
 
-    # Wrap properties in a `properties` key to match Thanos format
-    def to_representation(self, instance):
-        rep = super().to_representation(instance)
-        properties_keys = ["length", "is_palindrome", "unique_characters", 
-                           "word_count", "sha256_hash", "character_frequency_map"]
-        rep["properties"] = {k: rep.pop(k) for k in properties_keys}
-        return rep
+    def get_properties(self, obj):
+        return {
+            "length": obj.length,
+            "is_palindrome": obj.is_palindrome,
+            "unique_characters": obj.unique_characters,
+            "word_count": obj.word_count,
+            "sha256_hash": obj.sha256_hash,
+            "character_frequency_map": obj.character_frequency_map,
+        }
