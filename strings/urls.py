06328d17cb3f0712f2bc95f800810301d5d848ed
Goodnews Atekha Osas <154696005+GOODNEWS221@users.noreply.github.com>
2025-10-22 02:47:23 +0100

    Fix URL pattern for deleting strings

--- strings/urls.py
+++ strings/urls.py
@@ -10,21 +10,6 @@ urlpatterns = [
     path('strings/', StringListCreateView.as_view(), name='list_create_strings'),
     path('strings/<str:value>/', StringRetrieveView.as_view(), name='get_string'),
     path('strings/<str:value>/delete/', StringDeleteView.as_view(), name='delete_string'),
-    path('strings/filter-by-natural-language/', NaturalLanguageFilterView.as_view(), name='nlp_analysis'),
-]
-++++
-
-from django.urls import path
-from .views import (
-    StringListCreateView,
-    StringRetrieveView,
-    StringDeleteView,
-    NaturalLanguageFilterView,
-)
-
-urlpatterns = [
-    path('strings/', StringListCreateView.as_view(), name='list_create_strings'),
-    path('strings/<str:value>/', StringRetrieveView.as_view(), name='get_string'),
-    path('strings/<str:value>/', StringDeleteView.as_view(), name='delete_string'),  # Updated
+    path('strings/<str:value>/delete', StringDeleteView.as_view()),
     path('strings/filter-by-natural-language/', NaturalLanguageFilterView.as_view(), name='nlp_analysis'),
 ]
