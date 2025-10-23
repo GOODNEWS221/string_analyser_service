01c59c50e430e36ed340c986a22f40162956edab
Goodnews Atekha Osas <154696005+GOODNEWS221@users.noreply.github.com>
2025-10-23 19:20:28 +0100

    Update URL views to use API views

--- strings/urls.py
+++ strings/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 from .views import (
-    StringListCreateView,
-    StringRetrieveDeleteView,   # Combined GET & DELETE
-    NaturalLanguageFilterView
+    StringListCreateAPIView,
+    StringRetrieveDeleteAPIView,   # Combined GET & DELETE
+    StringNaturalLanguageFilterAPIView
 )
 
 urlpatterns = [
     # Create / List strings
-    path('strings/', StringListCreateView.as_view(), name='list_create_strings'),
+    path('strings/', StringListCreateAPIView.as_view(), name='list_create_strings'),
 
     # Retrieve single string or delete it
-    path('strings/<str:value>/', StringRetrieveDeleteView.as_view(), name='get_delete_string'),
+    path('strings/<str:value>/', StringRetrieveDeleteAPIView.as_view(), name='get_delete_string'),
 
     # Natural language filtering
-    path('strings/filter-by-natural-language/', NaturalLanguageFilterView.as_view(), name='nlp_analysis'),
+    path('strings/filter-by-natural-language/', StringNaturalLanguageFilterAPIView.as_view(), name='nlp_analysis'),
 ]
